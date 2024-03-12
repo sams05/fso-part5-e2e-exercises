2230853c21ae8147a74b3d3839067f2561b1b8e0
Samuel Shen <sshen05@terpmail.umd.edu>
2024-03-12 12:45:04 -0400

    Finish ex5.17

---
+++ tests/blog_app.spec.js
@@ -0,0 +1,21 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Note app', () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http:localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users', {
+      data: {
+        name: 'Matti Luukkainen',
+        username: 'mluukkai',
+        password: 'salainen',
+      },
+    })
+
+    await page.goto('http://localhost:5173')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    const loginForm = page.getByTestId('login-form')
+    await expect(loginForm).toBeVisible()
+  })
+})
