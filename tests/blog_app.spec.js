0e558b7678ae44dbbb5a8d23d0c16054749c7044
Samuel Shen <sshen05@terpmail.umd.edu>
2024-03-13 23:18:30 -0400

    Finish ex5.23

--- tests/blog_app.spec.js
+++ tests/blog_app.spec.js
@@ -107,5 +107,59 @@ describe('Blog app', () => {
         await expect(blogContainer.getByRole('button', { name: 'remove' })).not.toBeVisible()
       })
     })
+
+    describe('When user has several blogs with likes', () => {
+      const blogs = [
+        {
+          title: 'How to Blog',
+          author: 'Ron Bo',
+          url: 'blogginlife.net',
+          likes: 15,
+        },
+        {
+          title: 'Best Blog Ever',
+          author: 'John Doe',
+          url: 'example.com',
+          likes: 21,
+        },
+        {
+          title: 'Some Other Blog',
+          author: 'Bob Doe',
+          url: 'something.com',
+          likes: 4,
+        },
+        {
+          title: 'Not a Blog',
+          author: 'Nonblogger',
+          url: 'golb.com',
+          likes: 9,
+        },
+      ]
+      beforeEach(async ({ page }, testInfo) => {
+        testInfo.setTimeout(30000);
+        // Create blogs with the set number of likes
+        for (const blog of blogs) {
+          await helper.createBlog(page, blog)
+          const blogContainer = await helper.expandBlogByTitle(page, blog.title)
+          const blogLikes = blogContainer.getByText('likes')
+          const likeBtn = blogContainer.getByRole('button', { name: 'like' })
+          for (let i = 0; i < blog.likes; i++) {
+            const numLikesBefore = +(await blogLikes.textContent()).match(/\d+/)[0]
+
+            await likeBtn.click()
+
+            // Wait for number of likes to update
+            await blogLikes.filter({ hasText: `likes ${numLikesBefore + 1}` }).waitFor({ timeout: 10000 })
+          }
+        }
+      })
+      test('blogs are ordered with most likes first', async ({ page }) => {
+        const sortedBlogs = blogs.toSorted((blog1, blog2) => blog2.likes - blog1.likes)
+        const blogLocators = await page.getByTestId('blog').all()
+        for (let i = 0; i < blogLocators.length; i++) {
+          await expect(blogLocators[i]).toContainText(`likes ${sortedBlogs[i].likes}`)
+        }
+      })
+    })
   })
 })
